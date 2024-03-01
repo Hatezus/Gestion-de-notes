3daf3e8fd96fc69c0a41e750ee15fa464484d5d1
Hatezus <gurdil.lupus@gmail.com>
2024-03-01 05:21:03 +0100

    Added conditional display on no_active_year

--- Widgets/Display/display.py
+++ Widgets/Display/display.py
@@ -1,18 +1,22 @@
 from PySide6.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout
 
 from Widgets.No_Active_Year.no_active_year import NoActiveYearWidget
-
+from Modules.settings_module import load_settings
 
 class DisplayWidget(QWidget):
-     def __init__(self):
-         super().__init__()
+    def __init__(self):
+        super().__init__()
         
-         self.setMinimumHeight(500)
+        self.setMinimumHeight(500)
         
-         layout = QVBoxLayout()
+        layout = QVBoxLayout()
          
-         self.no_active_year_widget = NoActiveYearWidget()
+        self.settings = load_settings()
 
-         layout.addWidget(self.no_active_year_widget)
-         
-         self.setLayout(layout)
+        if self.settings["current_year"] == "None":
+
+            self.no_active_year_widget = NoActiveYearWidget()
+
+            layout.addWidget(self.no_active_year_widget)
+            
+        self.setLayout(layout)
