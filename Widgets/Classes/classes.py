a9a0b3fba167b5f398211e5b15eda26696af7f37
Hatezus <gurdil.lupus@gmail.com>
2024-03-09 10:29:05 +0100

    Added handling of classes path with classes buttons

--- Widgets/Classes/classes.py
+++ Widgets/Classes/classes.py
@@ -1,4 +1,6 @@
 from PySide6.QtWidgets import QWidget, QPushButton, QHBoxLayout, QButtonGroup
+from functools import partial
+from pathlib import Path
 
 import Modules.settings_module as settings_module
 
@@ -19,7 +21,7 @@ class ClassesWidget(QWidget):
             for class_name in settings["classes"]:
                 button = QPushButton(class_name)
                 button.setCheckable(True)
-                button.clicked.connect(self.buttonClicked)
+                button.clicked.connect(partial(self.buttonClicked, class_name))
                 self.classes_btn_group.addButton(button)
                 self.main_layout.addWidget(button)  # Ajoutez le bouton au layout principal
 
@@ -36,5 +38,9 @@ class ClassesWidget(QWidget):
             if widget is not None:
                 widget.deleteLater()
 
-    def buttonClicked(self, checked):
-        print("Bouton cliqué")
+    def buttonClicked(self, class_name):
+        settings = settings_module.load_settings()
+        current_class_path = Path(settings['current_year_path'])
+        current_class_path = current_class_path / class_name
+        print(current_class_path)
+        settings_module.update_settings('current_class_path', current_class_path.as_posix())
